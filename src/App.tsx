bc50e623d067b95bbedcd053fb57a4e662f7bf0c
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-26 01:18:28 +0430

    Implement routing and layout structure with Home page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
 
 export default function App() {
-
-  return <h1>Hello, World!</h1>
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
