9c382136cd479e8111d610605c969332fa6f78e2
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-26 01:25:51 +0430

    Add authentication routes and components for Login and Register

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
+import Register from './pages/Auth/Register'
+import Login from './pages/Auth/Login'
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
