d6b3efec3b2c105c5465c218b96be56e2bbd6eb6
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-26 15:16:56 +0430

    Update index.tsx to include Create post component in exports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,7 @@ import Layout from "./Layout";
 import Home from "./Home";
 import Register from "./Auth/Register";
 import Login from "./Auth/Login";
+import Create from "./Posts/Create";
 
-export { Layout, Home, Register, Login };
+
+export { Layout, Home, Register, Login, Create };
