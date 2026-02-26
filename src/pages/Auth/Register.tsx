4f73fb919b5f9055a187627a1ddd1d20f0d7de74
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-26 13:49:53 +0430

    Implement user authentication in Layout and Register component

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,7 +1,69 @@
+import { useState, type SyntheticEvent } from "react"
+import { useNavigate } from "react-router-dom";
+import useAppContext from "../../Context/useAppContext";
+
 export default function Register() {
+    const navigate = useNavigate();
+    const { setToken } = useAppContext();
+
+    const [formData, setFormData] = useState<{ name: string, email: string, password: string, password_confirmation: string, }>({
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+    })
+    const [errors, setErrors] = useState<{ name: string[], email: string[], password: string[] }>({ name: [], email: [], password: [] });
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const name = event.target.name
+        const value = event.target.value
+        setFormData({ ...formData, [name]: value })
+    }
+
+    async function onSubmit(event: SyntheticEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const response = await fetch("/api/register",
+            {
+                method: "post",
+                body: JSON.stringify(formData),
+            });
+
+        const data = await response.json();
+        if (data.errors) {
+            setErrors(data.errors)
+        } else {
+            localStorage.setItem("token", data.token)
+            setToken(data.token);
+            navigate("/");
+        }
+    }
+
     return (
         <>
             <h1 className="title">Register a new account</h1>
+
+            <form className="w-1/2 mx-auto space-y-6" onSubmit={onSubmit}>
+                <div>
+                    <input type="text" name="name" placeholder="Name" onChange={handleChange} value={formData.name} required />
+                    {errors.name && <p className="error">{errors.name}</p>}
+                </div>
+
+                <div>
+                    <input type="email" name="email" placeholder="Email" onChange={handleChange} value={formData.email} required />
+                    {errors.email && <p className="error">{errors.email}</p>}
+                </div>
+
+                <div>
+                    <input type="password" name="password" placeholder="Password" onChange={handleChange} value={formData.password} required />
+                    {errors.password && <p className="error">{errors.password}</p>}
+                </div>
+
+                <div>
+                    <input type="password" name="password_confirmation" placeholder="Confirm Password" onChange={handleChange} value={formData.password_confirmation} required />
+                </div>
+
+                <button className="primary-btn">Register</button>
+            </form>
         </>
     )
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
+import useAppContext from "../Context/useAppContext";
 
 export default function Layout() {
+    const { user } = useAppContext();
     return (
         <>
             <header>
                 <nav>
                     <Link to="/" className="nav-link">Home</Link>
-                    <div className="space-x-4">
-                        <Link to="/register" className="nav-link">Register</Link>
-                        <Link to="/login" className="nav-link">Login</Link>
-                    </div>
+                    {user && user.name ? (
+                        <div className="space-x-4">
+                            <p className="text-slate-400 text-xs">Welcome back {user.name}</p>
+                        </div>
+                    ) :
+                        <div className="space-x-4">
+                            <Link to="/register" className="nav-link">Register</Link>
+                            <Link to="/login" className="nav-link">Login</Link>
+                        </div>
+                    }
                 </nav>
             </header>
             <main>
